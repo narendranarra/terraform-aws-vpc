8de3478c85c853e698fc113bf2e8120a9ff47009
narendranarra <narendralinux47@gmail.com>
2025-07-09 16:31:46 +0530

    terraform

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,10 @@ output "vpc_id" {
   value       = aws_vpc.main.id
 }
 
+output "public_subnet_ids" {
+  value = aws_subnet.public[*].id
+}
+
 
 /* output "vpc_id" {
   value       = aws_vpc.main.id
