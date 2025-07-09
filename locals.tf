40c275d5ae5a2fa1b35121d859ff95879bcd6931
narendranarra <narendralinux47@gmail.com>
2025-07-09 16:36:54 +0530

    terraform

--- locals.tf
+++ locals.tf
@@ -4,7 +4,7 @@ locals {
     Environment = var.environment
     Terraform = "true"
   }
-#az_names = slice(data.aws_availability_zones.available.names, 0, 2)
+ az_names = slice(data.aws_availability_zones.available.names, 0, 2)
 }
 
 /* locals {
